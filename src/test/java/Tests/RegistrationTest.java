430ac8a899b7f917bfb61b66c1dfd143daac21b9
AmanKandhola <amankandhola01@gmail.com>
2024-01-18 16:20:41 +0530

    this is the commit in order to create conflict with skipAdd

--- src/test/java/Tests/RegistrationTest.java
+++ src/test/java/Tests/RegistrationTest.java
@@ -16,7 +16,7 @@ public class RegistrationTest extends BaseClass{
 		obj.SwitchIframe();
 		
 		utilObj.implictWait(10);
-		
+		System.out.print("This is the change in main branch");
 		//Thread.sleep(5000);
 		
 		obj.choseAdd();
